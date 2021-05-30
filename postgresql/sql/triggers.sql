e29e4b026a1304d7eeeea3f53ca493f4d234b1d9
Rodrigo Machado <rodkoalas@gmail.com>
2021-05-30 19:23:27 +0100

    Cancel auctions

--- postgresql/sql/triggers.sql
+++ postgresql/sql/triggers.sql
@@ -73,7 +73,7 @@ DECLARE
     new_bidder db_user.username%TYPE;
     id notifs.n_id%TYPE;
 BEGIN
-    IF old.last_bidder = new.last_bidder THEN
+    IF old.last_bidder is NULL OR old.last_bidder = new.last_bidder THEN
         RETURN NEW;
     END IF;
     
@@ -97,3 +97,54 @@ $$;
 DROP TRIGGER IF EXISTS outbidded ON auction;
 CREATE TRIGGER outbidded BEFORE UPDATE OF last_bidder ON auction
     FOR EACH ROW EXECUTE FUNCTION outbidded(); 
+
+-- This trigger notifies seller and bidders when auction is cancelled
+CREATE OR REPLACE FUNCTION cancelled() RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    cur CURSOR FOR
+        SELECT DISTINCT bidder FROM bid
+        WHERE auction_id = old.auction_id;
+    auction_seller INTEGER;
+    notify_date TIMESTAMP := CURRENT_TIMESTAMP;
+BEGIN
+    IF new.cancelled = false THEN
+        RETURN NULL;
+    END IF;
+
+    SELECT seller INTO STRICT auction_seller FROM auction
+    WHERE auction_id = old.auction_id;
+
+    CALL notify(auction_seller, notify_date,
+        'Auction ' || old.auction_id || ' has been cancelled. Sorry for any inconvenience.'
+    );
+
+    FOR row IN cur LOOP
+        IF NOT row.bidder = auction_seller THEN 
+            CALL notify(row.bidder, notify_date,
+                'Auction ' || old.auction_id || ' has been cancelled. Sorry for any inconvenience.'
+            );  
+        END IF;
+    END LOOP;
+
+    RETURN NULL;
+END;
+$$;
+
+DROP TRIGGER IF EXISTS cancelled on auction;
+CREATE TRIGGER cancelled AFTER UPDATE OF cancelled ON auction
+    FOR EACH ROW EXECUTE FUNCTION cancelled();
+
+CREATE OR REPLACE PROCEDURE notify(v_user INTEGER, v_date TIMESTAMP, v_msg VARCHAR)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    id INTEGER;
+BEGIN
+    SELECT COUNT(*)+1 INTO id FROM notifs WHERE user_id = v_user;
+    
+    INSERT INTO notifs (user_id, n_id, n_date, msg) 
+    VALUES (v_user, id, v_date, v_msg); 
+END;
+$$;
